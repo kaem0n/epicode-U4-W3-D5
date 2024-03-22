cafcece04a72622243923ac25694289d37e59a6f
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-03-22 09:19:57 +0100

    Initial commit

---
+++ src/test/java/kaem0n/ApplicationTest.java
@@ -0,0 +1,13 @@
+package kaem0n;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
